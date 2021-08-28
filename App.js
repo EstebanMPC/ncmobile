74543edb6e8d8ebc748633862970e26544e71f31
Esteban Porras <estebanporras@Estebans-MBP.fios-router.home>
2021-08-27 23:04:33 -0400

    mobileworking

--- App.js
+++ App.js
@@ -6,12 +6,3 @@ export default function App() {
         <Main />
     );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
